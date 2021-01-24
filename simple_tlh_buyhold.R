af6daed129f986ea51be56e3c43a716b3dcf7b7e
timelyportfolio <kent.russell@timelyportfolio.com>
2021-01-24 09:44:32 -0600

    add unrealized gl and add yearly return

--- simple_tlh_buyhold.R
+++ simple_tlh_buyhold.R
@@ -12,8 +12,10 @@ harvest <- function(dat = NULL, threshold = 0.1, portval = 100000) {
     price = rep(0, nrw),
     portval = rep(0, nrw),
     quantity = rep(0, nrw),
+    unrealgl = rep(0, nrw),
     yeargl = rep(0, nrw),
-    yearstartprice = rep(0, nrw)
+    yearstartprice = rep(0, nrw),
+    yearret = rep(0, nrw)
   )
 
   for(i in seq_len(nrow(dat_monthly))) {
@@ -34,17 +36,20 @@ harvest <- function(dat = NULL, threshold = 0.1, portval = 100000) {
       accum[i,"quantity"] <- lastrw[,"quantity"]
       accum[i,"yeargl"] <- lastrw[,"yeargl"] + accum[i,"gl"]
       accum[i,"yearstartprice"] <- accum[i-1,"yearstartprice"]
+      # harvest if price below threshold
       if(rw[,6,drop=TRUE] < lastrw$basis * (1 - threshold)) {
         accum[i,"basis"] <- rw[,6,drop=TRUE]
         accum[i,"gl"] <- (lastrw[,"quantity"] * rw[,6,drop=TRUE]) - lastrw[,"portval"]
         accum[i,"quantity"] <- accum[i,"portval"] / rw[,6,drop=TRUE]
         accum[i,"yeargl"] <- lastrw[,"yeargl"] + accum[i,"gl"]
       }
+      accum[i,"unrealgl"] <- accum[i, "portval"] - (accum[i, "quantity"] * accum[i, "basis"])
       # beginning of year reset yeargl
       if(months(accum[i,"date"]) == "January"){
         accum[i,"yeargl"] <- accum[i,"gl"]
         accum[i,"yearstartprice"] <- accum[i-1,"price"]
       }
+      accum[i-1,"yearret"] <- accum[i-1,"price"] / accum[i-1,"yearstartprice"] - 1
     }
   }
   
@@ -81,7 +86,7 @@ evaluate_harvesting <- function(dat = NULL) {
 ### try out our harvesting functions ----
 quantmod::getSymbols("^GSPC", from="1900-01-01")
 
-h <- test_harvest_yearly(GSPC["2000::",])
+h <- test_harvest_yearly(GSPC["2010::",])
 
 eh <- evaluate_harvesting(h)
 
